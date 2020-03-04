cd71ab133be8e142bd116fa73fe20cc739f6097c
Hassan Abbasi <hassan.abbp@gmail.com>
2020-03-04 17:25:11 +0330

    Add database | Set caption

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
+import sqlite3
+
 from pyrogram import Client, Message
 from pyrogram.client.ext.utils import decode_file_id
 from pyrogram.client.ext.base_client import BaseClient
 
 from config import *
+
+
+def exe_query(query):
+    con_obj = sqlite3.connect(DB_PATH)
+    courser = con_obj.execute(query)
+    res = courser.fetchall()
+    con_obj.commit()
+    con_obj.close()
+    return res
+
+
+def get_admin(telegram_id):
+    res = exe_query(f'SELECT * FROM Admin WHERE telegram_id = {telegram_id};')
+    return None if not res else res[0]
+
+
+def set_caption(admin: tuple, caption: str):
+    caption = caption.replace("'", '"')
+    exe_query(f"UPDATE Media SET caption = '{caption}' WHERE file_id = {admin[1]};")
+    exe_query(f'UPDATE Admin SET file_id = null WHERE telegram_id = {admin[0]};')
+
+
+def get_caption(file_id: str):
+    return 'caption'  # TODO: get caption from database
+
 
 client = Client(session_name='my_bot',
                 api_id=API_ID,
@@ -14,7 +41,8 @@ client = Client(session_name='my_bot',
 
 @client.on_message()
 def handle_message(bot: Client, msg: Message):
-    if msg.chat.id == ADMIN_ID and any([msg.document, msg.video, msg.photo, msg.audio, msg.voice]):
+    admin = get_admin(msg.chat.id)
+    if admin:
         if msg.document:
             file_id = msg.document.file_id
         elif msg.video:
@@ -25,8 +53,15 @@ def handle_message(bot: Client, msg: Message):
             file_id = msg.audio.file_id
         elif msg.voice:
             file_id = msg.voice.file_id
+        elif msg.text:
+            if admin[1]:
+                set_caption(admin, msg.text)
+                bot.send_message(msg.chat.id, 'Caption set successfully')
+            else:
+                bot.send_message(msg.chat.id, 'First, send a file')
+            return
         else:
-            bot.send_message(msg.chat.id, 'Never happen!')
+            bot.send_message(msg.chat.id, 'Send a valid file!')
             return
         bot.send_message(msg.chat.id, f't.me/{BOT_USERNAME}/?start={file_id}')
     elif msg.text and msg.text.startswith('/start '):
@@ -34,7 +69,7 @@ def handle_message(bot: Client, msg: Message):
         decode = decode_file_id(file_id)
         media_type = BaseClient.MEDIA_TYPE_ID[decode[0]]
         try:
-            bot.__getattribute__(f'send_{media_type}')(msg.chat.id, file_id)
+            bot.__getattribute__(f'send_{media_type}')(msg.chat.id, file_id, caption=get_caption(file_id))
         except AttributeError:
             bot.send_message(msg.chat.id, "Can't send file")
     else:
--- sample-config.py
+++ sample-config.py
@@ -1,3 +1,4 @@
+DB_PATH = 'db.sqlite'
 ADMIN_ID = 0
 BOT_USERNAME = ''
 API_ID = 0
